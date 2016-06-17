dcdcffc69423a2296a817fbdaf1aad7e7f6e3cd4
Andy Tang <andyt@live.nl>
2016-06-17 15:05:03 +0200

    extracted async handler

--- src/tag-loader.js
+++ src/tag-loader.js
@@ -1,24 +1,5 @@
-import fs from 'fs';
-
-function async(instance, ...args) {
-  return new Promise((resolve, reject) => {
-    // Handler to handle promises
-    function handler(data) {
-      const { value, next, done } = instance.next(data)
-      // When done notify all subscribers that we are done
-      if (done) {
-        resolve(value)
-        return
-      }
-      // There is no handling for non promise compatible interfaces, yet...
-      value.then(res => {
-        handler(res)
-      }, err => console.log(err)).catch(e => reject(e))
-    }
-    // Kick off the sequence
-    handler.apply(this, args)
-  });
-}
+import fs from 'fs'
+import async from './async'
 
 function getFileContents(folder) {
   return new Promise((resolve, reject) => {
@@ -33,8 +14,9 @@ function isDirectory(path) {
 }
 
 function *getTags(baseFolder) {
-  const items = yield getFileContents(baseFolder);
-  let tags = {};
+  const items = yield getFileContents(baseFolder)
+  let tags = {}
+
   for (var i = 0; i < items.length; i++) {
     let item = items[i]
     const isDir = yield isDirectory(`${baseFolder}/${item}`)
