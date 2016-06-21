5f57cd5b6db95b061ae8c6f20d1987d49023bb36
Andy Tang <andyt@live.nl>
2016-06-21 08:30:04 +0200

    using generator for async handling in app

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import riot from 'riot'
 import fs from 'fs'
 import tagLoader from './tag-loader'
+import async from './async'
 
 const app = express()
 const PORT = 80
@@ -14,17 +15,20 @@ function inject(content, page) {
 app.use(express.static(`${__dirname}/../.tmp`))
 app.use(express.static(`${__dirname}/../node_modules/riot`))
 
-tagLoader(`${__dirname}/tags`)
-  .then(tags => {
-    app.get('/:collection*?/:details*?/:action*?', (req, res) => {
-      const page = req.params.collection || 'home'
-      const tag = riot.render(tags['base-page.tag'], {
-        riot: riot,
-        page
-      })
-      const html = inject(tag, page)
-      res.send(html)
-    })
+function *startApp() {
+  const tags = yield tagLoader(`${__dirname}/tags`)
 
-    app.listen(PORT, () => console.log(`Server has started under port: ${PORT}`))
+  app.get('/:collection*?/:details*?/:action*?', (req, res) => {
+    const page = req.params.collection || 'home'
+    const tag = riot.render(tags['base-page.tag'], {
+      riot: riot,
+      page
+    })
+    const html = inject(tag, page)
+    res.send(html)
   })
+
+  app.listen(PORT, () => console.log(`Server has started under port: ${PORT}`))
+}
+
+async(startApp())
