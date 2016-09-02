86a9ca470ff8b08f7bf2bce6284eb698ba9ca8f1
Andy Tang <andyt@live.nl>
2016-09-02 21:00:33 +0200

    created state class

--- src/services/state.js
+++ src/services/state.js
@@ -1,5 +1,26 @@
 import stateResolver from './state-resolver'
 
+export class State {
+  constructor(initialState = this.getDefaultState()) {
+    this.state = initialState;
+  }
+
+  getState() {
+    return this.state;
+  }
+
+  setState(state) {
+    this.state = { ...this.state, ...state }
+  }
+
+  getDefaultState() {
+    return {
+      name: null,
+      page: 'home',
+    }
+  }
+}
+
 export default function createState(initialState = getDefaultState()) {
   return {
     state: initialState,
