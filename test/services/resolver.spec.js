c042d3bcc3ccb9d24a53f1ad57cf193b07420800
Andy Tang <andyt@live.nl>
2016-10-24 18:54:00 +0200

    added specs for resolving form data to services

---
+++ test/services/resolver.spec.js
@@ -0,0 +1,16 @@
+describe('resolver', () => {
+  describe('when resolving a form', () => {
+    describe('and a `POST` is made on the url: `/user`', () => {
+      describe('and the event is `add-user`', () => {
+        describe('and the form data contains `user.name`', () => {
+          it('should invoke the `add-user` event from `user`', () => {
+
+          })
+          it('should have a parameter with the interface: `{ user: { name: "" }}`', () => {
+
+          })
+        })
+      })
+    })
+  })
+})
