a25e5cca38b6fd50490b2cb9e049fba4a2d4233e
Andy Tang <andyt@live.nl>
2016-12-05 15:00:50 +0100

    extracted routes creation

--- src/app.js
+++ src/app.js
@@ -3,33 +3,16 @@ import bodyParser from 'body-parser'
 import RestIO from 'rest-io'
 import mongoose from 'mongoose';
 import riot from 'riot'
-import fs from 'fs'
-import tagLoader from './tag-loader'
 import async from './async'
 import user from './services/user'
 import login from './services/login'
 import todo from './services/todo'
-import { resolve, isServiceRegistered } from './services/resolver'
-import { convertFormData } from './services/formDataConverter'
+import { register } from './page-routes'
 
 const app = express()
 const jsonParser = bodyParser.json()
 const formParser = bodyParser.urlencoded({ extended: false })
 const PORT = 80
-
-function inject(content) {
-  const base = fs.readFileSync(`${__dirname}/index.html`, 'utf8')
-  return base.replace('{content}', content.toString())
-}
-
-function renderPage(state, tags) {
-  const tag = riot.render(tags['base-page.tag'], {
-    riot,
-    state
-  })
-  return inject(tag)
-    .replace('<base-page>', `<base-page state='{ ${JSON.stringify(state)} }'>`)
-}
 
 app.use(jsonParser)
 app.use(formParser)
@@ -41,33 +24,17 @@ process.env.REST_IO_HMAC_KEY = process.env.REST_IO_HMAC_KEY || 'hmac key which s
 process.env.REST_IO_AES_KEY = process.env.REST_IO_AES_KEY || 'aes key which is set via js to demo';
 
 function *startApp() {
-  const tags = yield tagLoader(`${__dirname}/tags`)
-
   app.get('/favicon.ico', (req, res) => res.send())
 
   new RestIO(app, {
     resources: __dirname + '/resources'
   });
 
-  app.get('/:page*?/:details*?/:action*?', (req, res) => {
-    const url = isServiceRegistered(req.url) ? req.url : '/login'
-    resolve(url, {}).then(state => {
-      res.send(renderPage(state, tags))
-    })
-  })
-
-  app.post('/:collection*?/:details*?/:action*?', (req, res) => {
-    const formData = new Map(Object.entries(req.body))
-    const data = convertFormData(formData)
-    resolve(req.url, data).then(state => {
-      res.send(renderPage(state, tags))
-    }, error => res.status(400).send(error))
-  })
+  register(app, '/login')
 
   mongoose.connect('mongodb://mongo:27017/test');
 
   app.listen(PORT, () => console.log(`Server has started under port: ${PORT}`))
 }
-
 
 async(startApp())
---
+++ src/page-routes.js
@@ -0,0 +1,43 @@
+import * as riot from 'riot'
+import async from './async'
+import tagLoader from './tag-loader'
+import fs from 'fs'
+import { resolve, isServiceRegistered } from './services/resolver'
+import { convertFormData } from './services/formDataConverter'
+
+function inject(content) {
+  const base = fs.readFileSync(`${__dirname}/index.html`, 'utf8')
+  return base.replace('{content}', content.toString())
+}
+
+function renderPage(state, tags) {
+  const tag = riot.render(tags['base-page.tag'], {
+    riot,
+    state
+  })
+  return inject(tag)
+    .replace('<base-page>', `<base-page state='{ ${JSON.stringify(state)} }'>`)
+}
+
+export function register(app, defaultRoute = '/') {
+  function *mountRoutes() {
+      const tags = yield tagLoader(`${__dirname}/tags`)
+
+      app.get('/:page*?/:details*?/:action*?', (req, res) => {
+        const url = isServiceRegistered(req.url) ? req.url : defaultRoute
+        resolve(url, {}).then(state => {
+          res.send(renderPage(state, tags))
+        })
+      })
+
+      app.post('/:collection*?/:details*?/:action*?', (req, res) => {
+        const formData = new Map(Object.entries(req.body))
+        const data = convertFormData(formData)
+        resolve(req.url, data).then(state => {
+          res.send(renderPage(state, tags))
+        }, error => res.status(400).send(error))
+      })
+    }
+
+    async(mountRoutes())
+}
